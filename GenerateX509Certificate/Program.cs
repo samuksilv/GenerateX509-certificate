4c13e17bb02d30abfc5162998c5e0640a2c7b064
Samuel Silva De Carvalho <samuel.carvalho@mag-it.com.br>
2019-09-20 12:19:48 -0300

    initial commit

---
+++ GenerateX509Certificate/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Org.BouncyCastle.X509;
+using Org.BouncyCastle.Utilities;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Asn1.X509;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Operators;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Pkcs;
+using System.IO;
+
+namespace GenerateX509Certificate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cert = GenerateCertificate(Guid.NewGuid().ToString());
+            Console.WriteLine(cert.ToString(true));            
+
+            string certificateName = "certificate-x509";
+            var filepath = Path.Join(Directory.GetCurrentDirectory(), $"{certificateName}.pfx");
+
+            File.WriteAllBytes(filepath, cert.Export(X509ContentType.Pfx));
+
+            //Path of certificate
+            Console.WriteLine(filepath);
+        }
+
+        public static X509Certificate2 GenerateCertificate(string subject)
+        {
+            var random = new SecureRandom();
+            var certificateGenerator = new X509V3CertificateGenerator();
+
+            var serialNumber = BigIntegers.CreateRandomInRange(BigInteger.One, BigInteger.ValueOf(Int64.MaxValue), random);
+            certificateGenerator.SetSerialNumber(serialNumber);
+
+            certificateGenerator.SetIssuerDN(new X509Name($"C=NL, O=SomeCompany, CN={subject}"));
+            certificateGenerator.SetSubjectDN(new X509Name($"C=NL, O=SomeCompany, CN={subject}"));
+            certificateGenerator.SetNotBefore(DateTime.UtcNow.Date);
+            certificateGenerator.SetNotAfter(DateTime.UtcNow.Date.AddYears(1));
+
+            const int strength = 2048;
+            var keyGenerationParameters = new KeyGenerationParameters(random, strength);
+            var keyPairGenerator = new RsaKeyPairGenerator();
+            keyPairGenerator.Init(keyGenerationParameters);
+
+            var subjectKeyPair = keyPairGenerator.GenerateKeyPair();
+            certificateGenerator.SetPublicKey(subjectKeyPair.Public);
+
+            var issuerKeyPair = subjectKeyPair;
+            const string signatureAlgorithm = "SHA256WithRSA";
+            var signatureFactory = new Asn1SignatureFactory(signatureAlgorithm, issuerKeyPair.Private);
+            var bouncyCert = certificateGenerator.Generate(signatureFactory);
+
+            // Lets convert it to X509Certificate2
+            X509Certificate2 certificate;
+
+            Pkcs12Store store = new Pkcs12StoreBuilder().Build();
+            store.SetKeyEntry($"{subject}_key", new AsymmetricKeyEntry(subjectKeyPair.Private), new[] { new X509CertificateEntry(bouncyCert) });
+            string exportpw = Guid.NewGuid().ToString("x");
+
+            using (var ms = new MemoryStream())
+            {
+                store.Save(ms, exportpw.ToCharArray(), random);
+                certificate = new X509Certificate2(ms.ToArray(), exportpw, X509KeyStorageFlags.Exportable);
+            }
+
+            Console.WriteLine($"Generated cert with thumbprint {certificate.Thumbprint}");
+
+            return certificate;
+        }
+    }
+}
